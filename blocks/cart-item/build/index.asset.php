5b6d76b23c681805738f554d6acecce2e6d05134
Puleeno Nguyen <puleeno@gmail.com>
2026-08-13 08:04:29 +0700

    ci: add release workflow

---
+++ blocks/cart-item/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => 'f03aba926a6a5c46a739');
---
+++ blocks/cart/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '919733924e8469b4276f');
