b35db40db625dfd95eef69412833ce829f8fb1ab
Puleeno Nguyen <puleeno@gmail.com>
2026-09-04 10:13:26 +0700

    feat: add default white background color to currency switcher block

--- blocks/currency-switcher/build/index.asset.php
+++ blocks/currency-switcher/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a613739b1814fcf8c788');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '963cdece1671c8d1e652');
