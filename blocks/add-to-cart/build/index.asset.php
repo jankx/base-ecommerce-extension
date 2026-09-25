25ec6acd8b869dde4cb73248c20990286f4a7b82
Puleeno Nguyen <puleeno@gmail.com>
2026-09-25 17:57:35 +0700

    fix(AddToCart): update asset version for consistency

--- blocks/add-to-cart/build/index.asset.php
+++ blocks/add-to-cart/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd63e99bbbb3a898c55f7');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '0d561c2c17450821a101');
