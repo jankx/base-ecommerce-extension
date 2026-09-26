bcf7083c17223b4b3b132687ecb2c69d909210d4
Puleeno Nguyen <puleeno@gmail.com>
2026-09-27 02:02:20 +0700

    fix(CurrencySwitcher): update minimum width for dropdown button for better responsiveness

--- blocks/currency-switcher/build/index.asset.php
+++ blocks/currency-switcher/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '2653115c6681a7fda04b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '4fbedcda4fc862a1480f');
